5f20e4835167d06c6df1b0fa901d32b536f66038
febbraiod <donedesign1@gmail.com>
2016-05-18 17:15:58 -0400

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,25 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ui.router'])
+    .config(function($stateProvider){
+      $stateProvider
+        .state('home', {
+          url:'/',
+          templateUrl: 'views/home.html',
+          controller: 'TwitterController'
+        })
+        .state('home.notifications', {
+          url: 'notifications',
+          templateUrl: 'views/home/notifications.html',
+          contoller: 'NotificationsController'
+        })   
+        .state('home.timeline', {
+          url: 'timeline',
+          templateUrl: 'views/home/timeline.html',
+          contoller: 'TimelineController'
+        }) 
+        .state('home.user', {
+          url: 'user',
+          templateUrl: 'views/home/user.html',
+          contoller: 'ProfileController'
+        });
+    });
